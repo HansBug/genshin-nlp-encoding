83184ff2717333c817b85240e2701f41ba3bfb0c
HansBug <hansbug@buaa.edu.cn>
2023-08-09 10:23:05 +0800

    dev(narugo): add gende.config

---
+++ gende/config/meta.py
@@ -0,0 +1,19 @@
+"""
+Overview:
+    Meta information for gende package.
+"""
+
+#: Title of this project (should be `gende`).
+__TITLE__ = "gende"
+
+#: Version of this project.
+__VERSION__ = "0.0.1"
+
+#: Short description of the project, will be included in ``setup.py``.
+__DESCRIPTION__ = 'Fine-tuning for genshin text.'
+
+#: Author of this project.
+__AUTHOR__ = "HansBug"
+
+#: Email of the authors'.
+__AUTHOR_EMAIL__ = "hansbug@buaa.edu.cn"
